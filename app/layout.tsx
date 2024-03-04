19d4340619fc0bdb097447cdb9c20fb637330757
Petter Sæther Moen <pettersmoen@gmail.com>
2024-03-04 22:48:21 +0100

    make title more personalized
    This will give the save as pdf print a better default name.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
-  title: "CV",
+  title: "Petter Sæther Moen - Software Engineer",
   description: "CV for Petter Sæther Moen",
 };
 
