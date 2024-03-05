62ca252471177dd25c606dc9cf5de44b5101a0d3
Petter Sæther Moen <pettersmoen@gmail.com>
2024-03-05 21:31:19 +0100

    add link to source code at bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { GitHubIcon } from "@/components/icons";
 
 export const metadata: Metadata = {
   title: "Petter Sæther Moen - Software Engineer",
@@ -19,10 +20,15 @@ export default function RootLayout({
       <body
         className={cn(
           GeistSans.className,
-          "m-0 min-h-screen w-full bg-gradient-to-br from-violet-100 via-purple-200 to-blue-200 text-slate-800 sm:p-4",
+          "m-0 flex min-h-screen w-full flex-col gap-8 bg-gradient-to-br from-violet-100 via-purple-200 to-blue-200 text-slate-800 sm:p-4 sm:pb-0",
         )}
       >
-        {children}
+        <div>{children}</div>
+        <div className="container flex max-w-4xl rounded-tl-sm rounded-tr-sm bg-white py-4 print:hidden">
+          <a href="https://github.com/petter/cv" className="group inline w-min">
+            <GitHubIcon className="h-6 w-6 text-slate-700 transition-colors group-hover:text-pink-800" />
+          </a>
+        </div>
         <Analytics />
       </body>
     </html>
