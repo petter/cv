9d07c576d78b037df2eb5cb249802917edad43e5
Petter Sæther Moen <pettersmoen@gmail.com>
2024-03-05 21:17:48 +0100

    tweak intro text in project section

--- app/projects.tsx
+++ app/projects.tsx
@@ -14,8 +14,8 @@ export function Projects() {
     <section className="space-y-4 print:break-inside-avoid">
       <h2 className="mb-4 text-xl font-bold">Projects</h2>
       <p className="max-w-lg text-pretty">
-        This is a list of all the projects I&apos;ve been involved in, either
-        through work or as a side project.
+        Here are som of the projects I&apos;ve been involved in, either through
+        work or as a side project.
       </p>
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 print:grid-cols-3">
         {RESUME_DATA.projects.map((project) => (
