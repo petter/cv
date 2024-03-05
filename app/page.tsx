fe347856a1dddf6fd9d24b4955778f96290eafcc
Petter Sæther Moen <pettersmoen@gmail.com>
2024-03-05 21:04:40 +0100

    less padding on small screens

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Education } from "./education";
 
 export default function Home() {
   return (
-    <main className="container relative max-w-4xl rounded-sm bg-white px-16 py-10 shadow-xl print:p-2 print:shadow-none">
+    <main className="container relative max-w-4xl rounded-sm bg-white px-6 py-10 shadow-xl sm:px-16 print:p-2 print:shadow-none">
       <div className="space-y-8">
         <Intro />
         <Separator />
